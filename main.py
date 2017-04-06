5b9f7cd7583f30035f629cdc3f468ddb0c74f007
Arnaud Lejeune <alejeune44@gmail.com>
2017-04-07 00:36:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import math
 from layer import Layer
-from layer import sigmoid
+from neuralNet import sigmoid
 from neuralNet import NeuralNet
 
-print ("-- TEST STRUCTURE et DISPLAY --")
-print ("\n")
-prev = Layer(5,5,None, "test")
-prev.output = [1, 0, 0, 0, 0]
-inp = Layer(5,5,prev,"Input")
-out = Layer(5,2,inp)
-
-print ("\n")
-print ("\n")
-
-print ("-- TEST RUN --")
-print ("\n")
-
-print ("\n")
-print ("\n")
-test = NeuralNet(300,100,6,3)
-test.addLayer(100,30)
-test.addLayer(30,10)
-test.addLayer(10,6)
+""" Crée et initialise un réseau de neurones à 3 Hidden Layers
+@param : coeff1 -> matrice des coefficients de la couche d'Input
+		 coeff2 -> matrice des coefficients de la première Hidden Layer
+		 coeff3 -> matrice des coefficients de la deuxième Hidden Layer
+		 coeff4 -> matrice des coefficients de la troisième Hidden Layer
+		 coeff5 -> matrice des coefficients de la couche d'Output
+@return : un NeuralNet prêt à être utilisé
+"""
+def initialize(coeff1, coeff2, coeff3, coeff4, coeff5):
+	network = NeuralNet(4624,1156,12,3)
+	network.Layers[0].coeff=coeff1
+	network.Layers[1].coeff=coeff5
+	network.addLayer(1156,300, coeff2)
+	network.addLayer(300,70,coeff3)
+	network.addLayer(70,12,coeff4)
+	return network
 
 
-print(test.run(300*[1]))
+
+test=initialize(np.ones((1156,4624)),np.ones((300,1156)),np.ones((70,300)),np.ones((12,70)),np.ones((3,12)))
+
+tableauInputs = 4624*[1]
+
+tableauOutputs = test.run(tableauInputs)
+
+
+test.run(4624*[1])
+test.display()
