b6bac42a01b9b827b8bc4e287ba53497c5bde9c2
Arnaud Lejeune <alejeune44@gmail.com>
2017-04-07 00:36:24 +0200

    Update layer.py

--- layer.py
+++ layer.py
@@ -1,8 +1,8 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import math
 
-def sigmoid(x):
-		return 1 / (1 + math.exp(-0.2*x))
 
 class Layer(object):
 
@@ -20,7 +20,6 @@ class Layer(object):
 		self.name = name+" ("+str(nbInput)+"x"+str(nbOutput)+")"
 		self.output = np.zeros(nbOutput)
 		self.coeff = np.ones((nbOutput,nbInput))
-		self.coeff[0] *= 2
 
 	"""calc() met à jour les ouput de __self__ en effectuant le produit matriciel input*coeff = output
 	@param: 
@@ -29,7 +28,7 @@ class Layer(object):
 	def calc(self):
 		raw_coeff = np.dot(self.coeff,self.prev.output)
 		for i in range(len(raw_coeff)):
-			self.output[i] = sigmoid(raw_coeff[i])
+			self.output[i] = raw_coeff[i]
 
 	"""toString() renvoie une String décrivant explicitement le Layer
 	@param: 
@@ -37,7 +36,7 @@ class Layer(object):
 	"""
 	def toString(self):
 		if(self.prev is None):
-			return ("-------BEGINNING OF INPUT LAYER: ---------------\nInput Layer, no coeffs.\nNumber of inputs: "+str(len(self.output))+"\n--------------END OF INPUT LAYER-------------")
+			return ("-------BEGINNING OF INPUT LAYER: ---------------\nInput Layer \nCoeffs\t" + str(self.coeff) + "\nOutput\t" + str(self.output) + "\n--------------END OF INPUT LAYER-------------")
 		return ("-------BEGINNING OF LAYER: "+self.name+" ---------------\nInput \t" + str(self.prev.output) + "\nCoeffs\t" + str(self.coeff) + "\nOutput\t" + str(self.output) + "\n--------------END OF LAYER: "+self.name+"-------------")
 
 
