192d2285d183f17cd67def39faeedcab6ce46314
Ojisama <ojisama.san@gmail.com>
2017-04-08 10:50:00 +0200

    Màj de dna.py et ajout de individu.py

--- dna.py
+++ dna.py
@@ -1,50 +1,46 @@
 import random
 
-
 class DNA():
-
-    def __init__(self, taille, rand=True):       
+    
+    def __init__(self, taille):       
         self.data=[]
-    if rand:
         for i in range(taille):
             self.data.append(random.random())
-    else:
-        for i in range(taille):
-                self.data.append(0)
+
 
     def crossover(self, ADN2, mutationcoeff):
-        new_adn = []
-        for i in range(len(self.data)):
-            if (i%2 == 0):
-                newcoeff = self.data[i]
-            else:
-                newcoeff = ADN2[i]
-        
-            if (random.random() < mutationcoeff):
-                print("mutation au rang " + str(i))
-            
-                hasard = random.randint(0,1)
-                changement = newcoeff*0.15
+		new_adn = DNA(len(self.data))
+		for i in range(len(self.data)):
+			if (i%2 == 0):
+				newcoeff = self.data[i]
+			else:
+				newcoeff = ADN2[i]
+		
+			if (random.random() < mutationcoeff):
+				#print("mutation au rang " + str(i))
+			
+				hasard = random.randint(0,1)
+				changement = newcoeff*0.15
 
-                if hasard == 0:
-                    if (newcoeff + changement) > 1:
-                        newcoeff = 1
-                    else:
-                        newcoeff += changement
-                else:
-                    if (newcoeff - changement) < 0:
-                        newcoeff = 0
-                    else:
-                        newcoeff -= changement
+				if hasard == 0:
+					if (newcoeff + changement) > 1:
+						newcoeff = 1
+					else:
+						newcoeff += changement
+				else:
+					if (newcoeff - changement) < 0:
+						newcoeff = 0
+					else:
+						newcoeff -= changement
 
-            new_adn.append(newcoeff)
-            
-        return(new_adn)
+			new_adn.data[i] = newcoeff
+			
+		return(new_adn)
 
 
-adn1 = DNA(5)
-adn2 = DNA(5)
-print(adn1.data)
-print(adn2.data)
-print(adn1.crossover(adn2.data,0.3))
-
+"""adn1 = DNA(5714020)
+adn2 = DNA(5714020)
+#print(adn1.data)
+#print(adn2.data)
+new_adn = adn1.crossover(adn2.data,0.3)
+#print(new_adn.data)"""
