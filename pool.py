f09cb01154221d772de5d098cdd7d32a8ef777fd
JMagloire <joalymagloire@hotmail.fr>
2017-04-14 11:50:47 +0200

    Update pool.py

--- pool.py
+++ pool.py
@@ -1,3 +1,7 @@
+from individu import Individu
+from collections import deque
+import random
+
 class Pool():
 
     """Population de (n) snakes a tester et a croiser
@@ -12,9 +16,37 @@ class Pool():
         self.n = n
         for i in range(self.n):
             self.population.append(Individu())
+        self.mutationcoeff = 1/self.getFitnessMax()
 
     def breeding(self):
-        return Individu()
+        # Creation du tableau de croisement
+        tab = []
+        fitnessMax = self.getFitnessMax()
+        for individu in self.population:
+            nb_apparition = int((individu.getFitness() * 100) / fitnessMax)
+            for i in range(nb_apparition):
+                tab.append(individu)
+
+        # Choix des deux parents
+        index1 = random.randint(0,len(tab)-1)
+        index2 = random.randint(0,len(tab)-1)
+        
+        while(index1 == index2): # eviter d'avoir le meme index
+            index2 = random.randint(0,len(tab)-1)
+        
+        print("index1 " + str(index1))
+        print("index2 " + str(index2))
+        
+        parent1 = tab[index1]
+        parent2 = tab[index2]
+
+        # Remplacer l'individu avec le fitness le plus faible par un enfant
+        index_pire_indiv = self.getFitnessMin()[1]
+        dna_enfant = parent1.dna.crossover(parent2.dna, self.mutationcoeff)
+        enfant = Individu(dna_enfant)
+        self.population[index_pire_indiv] = enfant
+
+        return enfant
         
 
     
@@ -25,8 +57,27 @@ class Pool():
                 fitnessMax = individu.getFitness()
         return fitnessMax
 
+    def getFitnessMin(self):
+        fitnessMin = 0
+        position_individuMin = 0
+        for i,individu in enumerate(self.population):
+            if individu.getFitness() < fitnessMin:
+                fitnessMin = individu.getFitness()
+                position_individuMin = i
+        return [fitnessMin,i]
+
     def getFitnessMoy(self):
         somme = 0
         for individu in self.population:
             somme += individu.getFitness()
         return somme/self.n
+
+    
+
+'''Tests
+
+# Test de la creation du tableau dans breeding
+a = Pool(2)
+a.population[0].size = 11
+print(a.getFitnessMax())
+a.breeding()"""
